29e131f788dfbf025ef5f1952f11c7f44030bef1
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-02-19 15:40:04 -0800

    first commit

---
+++ themes/inhabitant/inc/extras.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function inhabitant_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'inhabitant_body_classes' );
+
+//this function changes the logo to our inhabitant logo
+function inhabitant_logo() { ?>
+	<style type="text/css">
+		/* .login {
+		background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/home-hero.jpg);
+		background-size: cover;
+		background-repeat: no-repeat;
+		opacity: 0.5;
+		} */
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-text-dark.svg);
+		height:65px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+		padding-bottom: 30px;
+	
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'inhabitant_logo' );
+
+// this function changes the link of our inhabitant website
+function inhabitant_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'inhabitant_logo_url' );
+
+function inhabitant_logo_url_title() {
+    return 'Inhabitant Camping Supply Co.';
+}
+add_filter( 'login_headertitle', 'inhabitant_logo_url_title' );
+
