8ce0e6d1b25fbccce2a896309568b509578491ee
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-02-26 11:12:45 -0800

    css relative and absolute styles to entry header in journal

--- themes/inhabitant/index.php
+++ themes/inhabitant/index.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main default-index-page" role="main">
 			<div class="blog-container">
+				
 				<?php if ( have_posts() ) : ?>
 
 					<?php if ( is_home() && ! is_front_page() ) : ?>
@@ -19,9 +20,12 @@ get_header(); ?>
 					<?php endif; ?>
 
 					<?php /* Start the Loop */ ?>
+					
 					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'template-parts/content' ); ?>
+						
+							<?php get_template_part( 'template-parts/content' ); ?>
+							<a class="readmore" href="<?php echo get_permalink(); ?>"> Read More &rarr;</a>
+					
 
 					<?php endwhile; ?>
 
@@ -32,7 +36,7 @@ get_header(); ?>
 					<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 				<?php endif; ?>
-</div>
+			</div>
 <div class="sidebar">
 		<?php get_sidebar(); ?>
 		</div>
