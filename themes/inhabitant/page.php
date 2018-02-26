2ffd0340bbf1003c33edc2fc7b942e4d9c6ea36e
Paul Dudsdeemaytha <pauldudsdeemaytha@Pauls-MacBook-Pro.local>
2018-02-25 17:22:22 -0800

    added flex to page.php and styling to sidebar

--- themes/inhabitant/page.php
+++ themes/inhabitant/page.php
@@ -8,16 +8,17 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+		<main id="main" class="site-main default-template-page" role="main">
+			<div class="page-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-
+			</div>
+			<div class="sidebar">
+				<?php get_sidebar(); ?>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
